60f27242d3d45be387d793fcc6e4e0f2a7dafc0a
sam <coblesam360@gmail.com>
2023-01-09 15:47:48 -0800

    mini-batch code

--- train.jl
+++ train.jl
@@ -13,9 +13,10 @@ X_test .= hcat.(X_test, ones.(TYPE, k_test))
 k_train = size.(X_train, 1)
 X_train .= hcat.(X_train, ones.(TYPE, k_train))
 # @show size(X_test[1])
-y_test = loady("data/labels_test_1.txt")
+y_test_ = loady("data/labels_test_1.txt")
+y_test = unrolly(y_test_)
 (n,c) = size(y_train)
-t = size(y_test, 1)
+t = size(y_test_, 1)
 
 include("rnn.jl")
 
@@ -26,42 +27,55 @@ Wax = randn(nstate, (96 + 1))
 Wya = randn(c, nstate + 1)
 a0 = randn(nstate)
 
-maxIter = 10000
-stepSize = 1e-4
+BATCH_SIZE = 10
+MAX_ITERATIONS = Int(10000/BATCH_SIZE)
+STEP_SIZE = 1e-4
 
-for i in 1:maxIter
-	r = rand(1:n)
-	(f, gWaa, gWax, gWya, ga0) = bptt(Waa, Wax, Wya, a0, X_train[r], y_train[r,:])
-	global Waa = Waa - stepSize * gWaa
-	global Wax = Wax - stepSize * gWax
-	global Wya = Wya - stepSize * gWya
-	global a0 = ga0 - stepSize * ga0
-	if i % (maxIter/50) == 0
+for i in 1:MAX_ITERATIONS
+	f_mean = 0
+	gWaa_sum = zeros(TYPE, size(Waa))
+	gWax_sum = zeros(TYPE, size(Wax))
+	gWya_sum = zeros(TYPE, size(Wya))
+	ga0_sum = zeros(TYPE, size(a0))
+	for batch in 1:BATCH_SIZE
+		r = rand(1:n)
+		(f, gWaa, gWax, gWya, ga0) = bptt(Waa, Wax, Wya, a0, X_train[r], y_train[r,:])
+		f_sum += f
+		gWaa_sum += gWaa
+		gWax_sum += gWax
+		gWya_sum += gWya
+		ga0_sum += ga0
+	end
+	# r = rand(1:n)
+	# (f, gWaa, gWax, gWya, ga0) = bptt(Waa, Wax, Wya, a0, X_train[r], y_train[r,:])
+	global Waa = Waa - STEP_SIZE * gWaa_sum / BATCH_SIZE
+	global Wax = Wax - STEP_SIZE * gWax_sum / BATCH_SIZE
+	global Wya = Wya - STEP_SIZE * gWya_sum / BATCH_SIZE
+	global a0 = ga0 - STEP_SIZE * ga0_sum / BATCH_SIZE
+	if i % (MAX_ITERATIONS/30) == 0
 		correct = 0
+		test_err = 0
 		yhat = predict(X_test, Waa, Wax, Wya, a0)
 		for j in 1:t
-			if findmax(yhat[j])[2] - 1 == y_test[j]
+			test_err += sum((yhat - y_test).^2)
+			if findmax(yhat[j])[2] - 1 == y_test_[j]
 				correct += 1
 			end
 		end
+		test_err /= t
 		test_acc = correct/t
 		yhat = predict(X_train, Waa, Wax, Wya, a0)
 		correct = 0
+		train_err = 0
 		for j in 1:n
+			train_err += sum((yhat - y_train).^2)
 			if findmax(yhat[j])[2] - 1 == y_train_[j]
 				correct += 1
 			end
 		end
+		train_err /= n
 		train_acc = correct / n
-		@printf "ITERATION %d TRAIN ACCURACY:\t%f\n" i train_acc
-		@printf "ITERATION %d TEST ACCURACY:\t%f\n" i test_acc
-		@printf "ITERATION %d TRAINING ERROR:\t%f\n" i f
-		@printf "\n"
-		@show Waa
-		@show Wax
-		@show Wya
-		@show a0
-		@printf "\n\n\n\n"
+		@printf "ITERATION: %d\tTRAIN ACC: %f\tTEST ACC: %f\tTRAIN ERR: %f\tTEST ERR: %f\n" i train_acc test_acc train_err test_err
 	end
 end
 
