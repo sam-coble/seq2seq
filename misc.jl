bb591baaa9b1feae2ebd72d69a61ffce40dd4fab
sam <coblesam360@gmail.com>
2023-01-14 22:19:57 -0800

    types

--- misc.jl
+++ misc.jl
@@ -1,6 +1,4 @@
-SEPERATOR = "%%%"
-
-function loadX(T, filename)
+function loadX(T::DataType, filename::String)::Vector{Array{T, 2}} where T <: AbstractFloat
 	X = Vector{Array{T, 2}}()
 	f = open(filename)
 	for line in readlines(f)
@@ -10,20 +8,22 @@ function loadX(T, filename)
 	return X
 end
 
-function str2vec(T, str)
+function str2vec(T::DataType, str::String)::Array{T, 2} where T <: AbstractFloat
 	ret = Array{T, 2}(undef, length(str) + 1, 28)
 	i = 1
 	for ch in str
 		ret[i,:] = chr2vec(T, ch)
 		i += 1
 	end
-	EOS = zeros(T, 28)
-	EOS[28] = 1
-	ret[i] = EOS
+	ret[i] = begin
+		EOS = zeros(T, 28)
+		EOS[28] = 1
+		EOS
+	end
 	return ret
 end
 
-function chr2vec(T, char)
+function chr2vec(T::DataType, char::Char)::Vector{T} where T <: AbstractFloat
 	ret = zeros(T, 28)
 	ret[Int(char) - 96] = 1
 	return ret
